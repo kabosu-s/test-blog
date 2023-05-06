303ae16b0afdbb833dd2a2da14b40d6ea69c1e2d
e2saeki <saeki@e-2.co.jp>
2023-05-06 22:45:09 +0900

    GTMでGA４追加

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import '@/src/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', () => {
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+      gtag('config', 'GTM-NQ53ZCB')
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }
