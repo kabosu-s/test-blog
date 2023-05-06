0b8ee34927011466460c96b671e5ef0a9dc14c88
e2saeki <saeki@e-2.co.jp>
2023-05-06 23:17:58 +0900

    GPT４のやり方は多分パッケージ使うぽかったのでドキュメントにGTMを直接書いた

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,5 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
 import '@/src/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.events.on('routeChangeComplete', () => {
-      window.dataLayer = window.dataLayer || []
-      function gtag() {
-        dataLayer.push(arguments)
-      }
-      gtag('js', new Date())
-      gtag('config', 'GTM-NQ53ZCB')
-    })
-  }, [])
-
   return <Component {...pageProps} />
 }
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,10 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+      <script dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NQ53ZCB');`,
+                        }}/>
+      </Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <body>
+      <noscript dangerouslySetInnerHTML={{
+      __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=[GTM_ID]" height="0" width="0" style="display:none;visibility:hidden" />`,
+                    }}/>
         <Main />
         <NextScript />
       </body>
