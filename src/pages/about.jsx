a6ce949aa05246e5df2563cb7ab7f6fa25fe179c
e2saeki <saeki@e-2.co.jp>
2023-06-04 16:11:07 +0900

    strapiから画像を出力

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,12 +9,12 @@ const notoSansJapanese = Noto_Sans_JP({
   preload: false,
 })
 
-import styles from '@/src/styles/Notfound.module.scss';
+import styles from '@/src/styles/About.module.scss';
 import { Footer } from '@/src/components/Footer';
 import { Header } from '@/src/components/Header';
 
-export default function Page ({data}) {
-  if (!data || data.length === 0) {
+export default function Page ({posts, apiUrl}) {
+  if (!posts || posts.length === 0) {
     return <div>Loading...</div>;
   }
   return (
@@ -25,44 +25,44 @@ export default function Page ({data}) {
       </Head>
       <Header />
       <main className={`${styles.main} ${notoSansJapanese.className}`}>
-      <div className={`${styles.main_visual}`}>
-        {data.map((item) => (
-          <div key={item.id}>
-            <h1>{item.attributes.pagename}</h1>
-            <div dangerouslySetInnerHTML={{ __html: item.attributes.content }} />
+        {posts.map((post) => (
+          <div key={post.id} className={`${styles.main_visual}`}>
+            <img src={`${post?.attributes?.viewimage?.data?.attributes?.url}`} alt="" />
+            <h1>{post.attributes.pagename}</h1>
+            <div className={`${styles.content}`} dangerouslySetInnerHTML={{ __html: post.attributes.content }} />
           </div>
         ))}
-      </div>
       </main>
       <Footer />
     </>
   );
 }
 
-
 //データを読み込む
 export async function getStaticProps() {
   try {
     // StrapiのエンドポイントURLを指定
-    const apiUrl = 'https://strapi-production-65d6.up.railway.app/api/static-pages'; 
-
+    const apiUrl = 'https://strapi-production-65d6.up.railway.app';
+    // ローカル（画像のパスがホストからじゃないのでapiURLを渡すこと）
+    // const apiUrl = 'http://localhost:1337';
+    const endpoint = apiUrl + '/api/static-pages?populate=*'
     // Strapiからデータを取得
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(endpoint);
     // レスポンスのデータを取り出す
-    const data = response.data?.data;
+    const posts = response.data?.data;
 
-    console.log(data); // データの中身を確認
-
+    console.log(posts); // データの中身を確認
     return {
       props: {
-        data,
+        posts,
+        apiUrl,
       },
     };
   } catch (error) {
-    console.error('Error fetching Strapi data:', error);
+    console.error('えらー！ fetching Strapi data:', error);
     return {
       props: {
-        data: null, 
+        posts: [],
       },
     };
   }
